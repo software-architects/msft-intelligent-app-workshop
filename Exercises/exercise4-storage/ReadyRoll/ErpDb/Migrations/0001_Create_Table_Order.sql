b1f95ca808f53a3d0dd693c62e7f93acec44207f
Rainer Stropek <rainer@software-architects.at>
2017-08-30 10:43:06 +0200

    Add ReadyRoll project

---
+++ Exercises/exercise4-storage/ReadyRoll/ErpDb/Migrations/0001_Create_Table_Order.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="41e3294c-0449-454d-9db8-d746d3d11d45" />
+GO
+
+PRINT N'Creating [dbo].[Order]'
+GO
+CREATE TABLE [dbo].[Order]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[Amount] [int] NOT NULL,
+[Customer] [nvarchar] (max) NULL,
+[Product] [nvarchar] (max) NULL,
+[TotalAmount] [decimal] (18, 2) NOT NULL,
+[UnitPrice] [decimal] (18, 2) NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_Order] on [dbo].[Order]'
+GO
+ALTER TABLE [dbo].[Order] ADD CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED  ([ID])
+GO
---
+++ Exercises/exercise4-storage/ReadyRoll/ErpDb/Migrations/0002_Create_Table_Product.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="b588b798-ba16-4c3b-b2d6-250b4c2503cd" />
+GO
+
+PRINT N'Creating [dbo].[Product]'
+GO
+CREATE TABLE [dbo].[Product]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[ProductCode] [nvarchar] (20) NOT NULL,
+[ProductDescription] [nvarchar] (max) NULL,
+[UnitPrice] [decimal] (18, 2) NULL
+)
+GO
