73f00d2f1fcd160af19c0c554c61e9b5d69a2263
Rainer Stropek <rainer@software-architects.at>
2017-08-30 16:22:48 +0200

    Add .NET samples for Docker

---
+++ Exercises/exercise5-containers/NetCore/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+        public static IWebHost BuildWebHost(string[] args) => 
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
+        public void Configure(IApplicationBuilder app) => app.UseMvc();
+    }
+
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() => new [] { "Hello", "World" };
+    }
+}
---
+++ Exercises/exercise5-containers/NetFx/NetFx/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Collections.Generic;
+
+[assembly: OwinStartup(typeof(NetFx.Startup))]
+
+namespace NetFx
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.MapHttpAttributeRoutes();
+            app.UseWebApi(configuration);
+        }
+
+        public class HelloController : ApiController
+        {
+            [HttpGet]
+            [Route("api/hello")]
+            public IEnumerable<string> Get() => new[] { "Hello", "World" };
+        }
+    }
+}
