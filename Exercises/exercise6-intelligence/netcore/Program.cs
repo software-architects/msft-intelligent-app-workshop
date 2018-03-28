2aa10016f2f772184d48e2516aac7895262f9ab0
Rainer Stropek <rainer@software-architects.at>
2018-03-28 14:49:19 +0300

    Update to Face API

--- Exercises/exercise6-intelligence/netcore/Program.cs
+++ Exercises/exercise6-intelligence/netcore/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,9 +12,11 @@ namespace EmotionApi
     {
         static async Task Run()
         {
-            var emotionService = new EmotionServiceClient("524b2093a8d84cddae93714a9d8edd96");
-            var emotions = await emotionService.RecognizeAsync(File.OpenRead(@"C:\Code\GitHub\msft-intelligent-app-workshop\Exercises\exercise6-intelligence\images\rainer.jpg"));
-            if (emotions.FirstOrDefault().Scores.Happiness > 0.99)
+            var emotionService = new FaceServiceClient("fd9e1c352a6a42eaa06e1babc9ce64b1", "https://northeurope.api.cognitive.microsoft.com/face/v1.0");
+            var emotions = await emotionService.DetectAsync(
+                File.OpenRead(@"C:\Code\GitHub\msft-intelligent-app-workshop\Exercises\exercise6-intelligence\images\rainer.jpg"),
+                returnFaceAttributes: new [] { FaceAttributeType.Smile });
+            if (emotions.FirstOrDefault().FaceAttributes.Smile > 0.99)
             {
                 Console.WriteLine(":-)");
             }
